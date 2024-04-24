8b5652eae1f08a1ffa2e93a169c5804999bb7527
Ridhwankiki <ridhwanr0607@gmail.com>
2024-04-24 13:18:59 +0700

    Initial Commit

---
+++ VoicesAI/VoicesAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoicesAIApp.swift
+//  VoicesAI
+//
+//  Created by MacBook Pro on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct VoicesAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
