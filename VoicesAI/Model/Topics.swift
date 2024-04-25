805404002b5ff273d8df067434eff418b8dfd790
Ridhwankiki <ridhwanr0607@gmail.com>
2024-04-25 11:26:11 +0700

    Bug Fixing and Housekeeping pickerView to solve selectedTag Picker raw value

--- VoicesAI/Model/Topics.swift
+++ VoicesAI/Model/Topics.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 enum Topics: String, CaseIterable {
-    case persahabatan
-    case cinta
-    case motivasi
-    case horor
+    case persahabatan = "Persahabatan"
+    case romantis = "romantis"
+    case motivasi = "motivasi"
+    case horor = "horor"
 }
 
 enum Mood: String, CaseIterable {
-    case bahagia
-    case sedih
+    case bahagia = "bahagia"
+    case sedih = "sedih"
 }
--- VoicesAI/ViewModel/StoryVM.swift
+++ VoicesAI/ViewModel/StoryVM.swift
@@ -6,5 +6,43 @@
 //
 
 import Foundation
+import GoogleGenerativeAI
 
+@MainActor
+class StoryVM: ObservableObject {
+    @Published var storyText = ""
+    @Published var isLoading = false
+    @Published var errorMessage: String?
+    
+    // MARK: - GENERATE STORY
+    
+    func generateStory(topic: Topics, mood: Mood) async {
+        isLoading = true
+        defer { isLoading = false }
+        errorMessage = nil
+        storyText = ""
+        
+        let apiKey = UserDefaults.standard.string(forKey: "GeminiAIKey") ?? ""
+        
+        guard !apiKey.isEmpty else {
+            errorMessage = "API KEY is missing. Please set it in Setting Page!"
+            return
+            
+        }
+        let model = GenerativeModel(name: "gemini-pro", apiKey: apiKey)
+        let topicString = topic.rawValue.capitalized
+        let moodString = mood.rawValue.capitalized
+        
+        let prompt = "Tuliskan Quote atau kutipan tentang \(topicString) dengan mood emosi \(moodString)."
+        
+        do {
+            let response = try await model.generateContent(prompt)
+            if let text = response.text {
+                storyText = text
+            }
+        } catch {
+            errorMessage = "Failed to generetae story: \(error.localizedDescription)"
+        }
+    }
 
+}
